e7b3093df97c0dcad9f808d4803b97717f486c32
Ritika <46516920+ritikakalra2000@users.noreply.github.com>
2022-03-25 18:06:05 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import pickle
+from socialbot import Bot
+pickle_in=open('bot.pickle','rb')
+bot=pickle.load(pickle_in)
+
+
+# In[7]:
+
+
+import fastapi
+
+
+# In[9]:
+
+
+from fastapi import FastAPI,Request
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+import secrets
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import json
+from selenium.common.exceptions import ElementClickInterceptedException
+
+
+# In[10]:
+
+
+app=FastAPI()
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    i=0
+    b=" "
+    while(i<len(exc.errors())) :
+        if (exc.errors()[i]['type']=="type_error.float"):
+            b=b+"    "+ exc.errors()[i]['loc'][1]+ " has been given wrong data type"
+        elif (exc.errors()[i]['type']=="value_error.missing"):
+            b=b+"    "+ exc.errors()[i]['loc'][1]+" is missing"
+        else:
+            b=exc.errors()
+        i=i+1
+    return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            content=jsonable_encoder({"detail": b}),
+        )
+
+class Item(BaseModel):
+    user: str
+    passw: str
+    hasht: str
+@app.put("/insta")
+async def instag(item: Item):
+    recieved=item.dict()
+    action=bot(recieved['user'],recieved['passw'],recieved['hasht'])
+    
+    return {action}
+
+
+# In[ ]:
+
+
+
+
---
+++ socialbot.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.keys import Keys
+from random import randint
+from selenium.common.exceptions import NoSuchElementException
+
+class Bot():
+    a=0
+    links = []
+
+    comments = ['great','nice','awesome'
+    ]
+
+    def __new__(self,use,passw,hasht):
+        l=self.login(self,use,passw)
+        if(l!="done"):
+            return l
+        self.like_comment_by_hashtag(self,hasht)
+        return "done"
+
+    def login(self, username, password):
+        self.driver = webdriver.Chrome(executable_path='C:/Users/Ritika/AppData/Local/chromedriver.exe')
+        try:
+            self.driver.get('https://instagram.com/')
+            sleep(5)
+            username_input = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input')
+            username_input.send_keys(username)
+            sleep(1)
+            password_input = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input')
+            password_input.send_keys(password)
+            sleep(1)
+        
+            self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button').click()
+            sleep(3)
+            self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click() # clicking 'not now btn'
+        except :
+            err=self.driver.find_element_by_xpath('//*[@id="loginForm"]/div[2]/p').text
+            self.driver. close()
+            return err
+        sleep(2)
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click() # clicking 'not now btn'
+        return "done"
+    def like_comment_by_hashtag(self, hashtag):
+        self.driver.get('https://www.instagram.com/explore/tags/'+hashtag)
+        
+        links = self.driver.find_elements_by_tag_name('a')
+
+        def condition(link):
+            return '.com/p/' in link.get_attribute('href')
+        valid_links = list(filter(condition, links))
+
+        for i in range(5):
+            link = valid_links[i].get_attribute('href')
+            if link not in self.links:
+                self.links.append(link)
+
+        for link in self.links:
+            self.driver.get(link)
+            # like
+            sleep(5)
+            self.driver.find_element_by_xpath("//span[@class='fr66n']").click()
+            sleep(10)
+
+            # comment
+            self.driver.find_element_by_class_name('RxpZH').click() 
+            sleep(5)
+            self.driver.find_element_by_xpath("//textarea[@placeholder='Add a comment…']").send_keys(self.comments[randint(0,1)])
+            sleep(5)
+            self.driver.find_element_by_xpath("//button[@type='submit']").click()
+
+
+
+
+
